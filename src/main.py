171df2f3e99f4e902bde1a7a792cdcf297e287da
RebornFlamme <85119428+RebornFlamme@users.noreply.github.com>
2023-03-28 17:16:27 +0200

    Connection function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print('Hello World')
+from func_connections import connect_dydx
+from datetime import datetime
+
+
+if __name__ == "__main__":
+
+    # Connect to client 
+    try:
+        client = connect_dydx()
+    except Exception as e:
+        print(e)
+        print("Error connecting to client:", e)
+        exit(1)
+
+
